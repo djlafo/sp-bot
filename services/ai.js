c96a7b7f2c8409eb0bd3335d63b0dc9c00196656
Dylan <djlafo@gmail.com>
2025-12-04 13:39:51 -0600

    update model

--- services/ai.js
+++ services/ai.js
@@ -84,7 +84,7 @@ export const replyToMessage = async (message, character, bot) => {
     try {
         const chatCompletion = await chatGPT.chat.completions.create({
             messages: lastMessages,
-            model: 'x-ai/grok-4.1-fast:free:online',
+            model: 'x-ai/grok-4.1-fast:online',
             stream: true,
             // tools: tools,
             plugins: [
